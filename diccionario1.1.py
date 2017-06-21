041ed0ca6219eaa1943093b7507312284e0b82ac
Udem <udem@fedora.mshome.net>
2017-06-21 17:24:09 -0600

    modificado

--- diccionario1.1.py
+++ diccionario1.1.py
@@ -7,18 +7,17 @@ if __name__=='__main__':
     parser.add_argument('edad')
     parser.add_argument('ciudad')
     parser.add_argument('curso')
- 	
+    
     args = parser.parse_args()
     datos={
- 	    'nombre':args.nombre,
- 	    'edad':args.edad,
- 	    'ciudad':args.ciudad,
- 	    'curso':args.curso,
-	}
+        'nombre':args.nombre,
+        'edad':args.edad,
+        'ciudad':args.ciudad,
+        'curso':args.curso,
+    }
 
     mensaje = 'El estudiante {nombre} con la edad de: {edad}, y que habita en la ciudad de {ciudad} y cursa el {curso} anio'
 
     print mensaje.format(nombre='asdf', edad='asdf' , ciudad='asdf',curso='asdf')
     for llave,valor in datos.iteritems():
         print llave,valor
-
