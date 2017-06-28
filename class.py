4d292db2ff8e6d203884ae771cf7f5310393307d
Udem <udem@fedora.mshome.net>
2017-06-28 17:48:12 -0600

    primera clase

---
+++ class.py
@@ -0,0 +1,17 @@
+class Animales(object):
+    def __init__(self, perro, gato):
+        self.perro = perro
+        self.gato =gato
+
+    def encontrar_nombre(self):
+        return '{perro} {gato}'.format(
+            perro=self.perro,
+            gato=self.gato
+        )
+class Mascotas(object):
+    def __init_(self,id):
+        self.id=id
+def obtener_id(self):
+   class mascotas(Animales,Mascotas):
+    def __init__(self,hogar):
+        self.hogar=hogar
