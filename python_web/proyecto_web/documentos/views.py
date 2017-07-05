98e1f2b12436aa5c102f58a04d7ea567c9373f5b
Udem <udem@fedora.mshome.net>
2017-07-05 17:50:36 -0600

    proyecto_web

--- python_web/proyecto_web/documentos/views.py
+++ python_web/proyecto_web/documentos/views.py
@@ -4,3 +4,25 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import View
+#
+from django.shortcuts import render
+##crear vista de documentos -definir metodo get
+from documentos.models import Documento
+class documentos(views):
+	"""docstring for ClassName"""
+
+	def get(self, request, *args, **kwargs):
+		docs = Documento.objects.all()
+		#Select *From documentos_Documento;
+		context = {
+		    'docs':docs,
+		    'encabezado':'Mis documentos'
+		}
+ 
+		return render(request,
+			'base.html',
+		 context)
+
+
+
