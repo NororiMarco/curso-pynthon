b03c715be11ee3ad40c4a6c220fb432be66d831c
Udem <udem@fedora.mshome.net>
2017-07-12 17:55:01 -0600

    proceso

---
+++ python-gtk/vent.py
@@ -0,0 +1,47 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class Mi_Ventana(Gtk.Window):
+	def __init__(self, *args, **kwargs):
+		
+		super(Mi_Ventana, self).__init__(*args, **kwargs)
+		self.set_size_request(500, 300)
+		self.connect('delete-event', Gtk.main_quit)
+
+		self.agregar_boton()
+		self.agregar_texto()
+		self.agregar_label()
+		self.agregar_salida()
+		self.contenedor()
+
+	def Imprimir(self, boton):
+		impreso = self.texto.get_text()
+		self.label_1.set_text(impreso)
+
+	def agregar_boton(self):	
+		self.boton_1 = Gtk.Button('Resaltar texto escrito')
+		self.boton_1.connect('clicked', self.Imprimir)
+
+	def agregar_texto(self):	
+		self.texto = Gtk.Entry()
+
+	def agregar_label(self):	
+		self.label_1 = Gtk.Label('Mostrar el texto')
+
+	def agregar_salida(self):	
+		self.botonexit = Gtk.Button('Finalizar')
+		self.botonexit.connect('clicked', Gtk.main_quit)
+
+	def contenedor(self):	
+		self.box = Gtk.VBox()
+		self.box.pack_start(self.texto, True, False, 0)
+		self.box.pack_start(self.boton_1, True, False, 0)
+		self.box.pack_start(self.label_1, True, False, 0)
+		self.box.pack_start(self.botonexit, True, False, 0)
+		self.add(self.box)
+
+if __name__ == '__main__':			
+	ventana = Mi_Ventana()
+	ventana.show_all()
+Gtk.main()
